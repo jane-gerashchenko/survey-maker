06fa53ac7c493f077e83cca95148c730d919f0aa
Ievgeniia Taranenko <ievgeniia.taranenko@adidas.com>
2022-06-13 11:59:05 +0200

    changed message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
+    res.send({ test: 'first change' });
 });
 
 const PORT = process.env.PORT || 5000;
