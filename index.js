daafa59da5d51b6edea2786516561aed35f09286
Ievgeniia Taranenko <ievgeniia.taranenko@adidas.com>
2022-06-14 15:53:33 +0200

    index.js

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI).then(() => {
+    console.log('Connected...')
+}).catch((e) => {
+    console.log('Connection error', e.message)
+});
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ test: 'first change' });
-});
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
