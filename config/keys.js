ac4ec183a0500d3d006c3a60b5625f032275f241
Ievgeniia Taranenko <ievgeniia.taranenko@adidas.com>
2022-06-15 17:07:03 +0200

    added configs for prod

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+if (process.env.NODE_ENV === 'production') {
+    // return prod set of keys
+    module.exports = require('./prod');
+} else {
+    // return dev set of keys
+    module.exports = require('./dev');
+}
