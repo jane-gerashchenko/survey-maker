08d4694426cf0c8231657ebaacf0d05eb46ae709
Ievgeniia Taranenko <ievgeniia.taranenko@adidas.com>
2022-06-22 14:29:11 +0200

    installed materialize css for styling created header component

---
+++ client/src/components/Header.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+    render() {
+        return (
+            <nav>
+                <div className="nav-wrapper">
+                    <a className="left brand-logo">
+                        Survey maker
+                    </a>
+                    <ul className="right">
+                        <li>
+                            <a>Login with Google</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        );
+    }
+}
+
+export default Header
