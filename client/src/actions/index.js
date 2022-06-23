9359956b58fce660d1215276e373c0bc22fb8446
Ievgeniia Taranenko <ievgeniia.taranenko@adidas.com>
2022-06-23 10:44:37 +0200

    modified auth reducer to return 3 values so that store knows if user is logged in

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,6 +3,6 @@ import { FETCH_USER } from './types';
 
 export const fetchUser = () => async dispatch => {
     const res = await axios.get('/api/current_user')
-    
-    dispatch({ type: FETCH_USER, payload: res });
+
+    dispatch({ type: FETCH_USER, payload: res.data });
 };
