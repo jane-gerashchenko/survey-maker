2aae3d1473c15be97cc51fb1e7eb65a2c0311ba5
Ievgeniia Taranenko <ievgeniia.taranenko@adidas.com>
2022-06-24 13:23:54 +0200

    added description for the payment pop up

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -5,6 +5,8 @@ class Payments extends Component {
     render() {
         return (
             <StripeCheckout
+                name="Survey maker"
+                description="5$ for 5 survey credits"
                 amount={500}
                 token={token => console.log('token ', token)}
                 stripeKey={process.env.REACT_APP_STRIPE_KEY}
