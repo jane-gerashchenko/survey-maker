423feb3e7a5b7a30c61f1a59a705fe35c40b52f6
Ievgeniia Taranenko <ievgeniia.taranenko@adidas.com>
2022-06-24 13:16:44 +0200

    Payment component using react-stripe-checkout

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import Payments from "./Payments";
 
 class Header extends Component {
     renderContent() {
@@ -12,9 +13,10 @@ class Header extends Component {
                     <li><a href="/auth/google">Login with Google</a></li>
                 );
             default:
-                return (
+                return [
+                    <li><Payments /></li>,
                     <li><a href="/api/logout">Log out</a></li>
-                );
+                ];
         }
     }
 
