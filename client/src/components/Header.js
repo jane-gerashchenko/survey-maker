c24c23c548e6d66f7469f7c3b242cd9f47682ea9
Ievgeniia Taranenko <ievgeniia.taranenko@adidas.com>
2022-06-28 10:28:09 +0200

    showing credits in the header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -15,7 +15,10 @@ class Header extends Component {
             default:
                 return [
                     <li key="1"><Payments /></li>,
-                    <li key="2"><a href="/api/logout">Log out</a></li>
+                    <li key="2" style={{ margin: '0 10px' }}> 
+                        Credits:{this.props.auth.credits}
+                    </li>,
+                    <li key="3"><a href="/api/logout">Log out</a></li>
                 ];
         }
     }
