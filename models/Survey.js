1a7b178dea81fe80cdd1574d7d16134ae11f1e9b
Ievgeniia Taranenko <ievgeniia.taranenko@adidas.com>
2022-06-28 16:43:34 +0200

    additional fields for survey schema

--- models/Survey.js
+++ models/Survey.js
@@ -9,7 +9,9 @@ const surveySchema = new Schema ({
     recipients: [RecipientSchema],
     yes: { type: Number, default: 0 },
     no: { type: Number, default: 0 },
-    _user: { type: Schema.Types.ObjectId, ref: 'User'}
+    _user: { type: Schema.Types.ObjectId, ref: 'User'},
+    dateSent: Date,
+    lastResponded: Date
 });
 
 mongoose.model('surveys', surveySchema);
