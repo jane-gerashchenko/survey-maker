ed7ca2ca026440546450c06b4879a0d58ed9b2c1
Ievgeniia Taranenko <ievgeniia.taranenko@adidas.com>
2022-06-29 14:16:30 +0200

    implemented sending sendGrid email

--- services/Mailer.js
+++ services/Mailer.js
@@ -6,6 +6,7 @@ class Mailer extends helper.Mail {
     constructor({ subject, recipients }, content) {
         super();
 
+        this.sgApi = sendgrid(keys.sendGridKey);
         this.from_email = new helper.Email('no-reply@surveymaker.com');
         this.subject = subject;
         this.body = new helper.Content('text/html', content);
@@ -32,11 +33,22 @@ class Mailer extends helper.Mail {
 
     addRecipients() {
         const personalize = new helper.Personalization();
-        
+
         this.recipients.forEach(recipients => {
             personalize.addTo(recipients);
         });
         this.addPersonalization(personalize);
+    }
+
+    async send() {
+        const request = this.sgApi.emptyRequest({
+            method: 'POST',
+            path: '/v3/mail/send',
+            body: this.toJSON()
+        });
+
+        const response = this.sgApi.API(request);
+        return response;
     }
 }
 
