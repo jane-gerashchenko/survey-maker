3e7a826223266e0351f864cb49015d1f10a8b895
Ievgeniia Taranenko <ievgeniia.taranenko@adidas.com>
2022-06-29 15:06:51 +0200

    sending an email

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 const Survey = mongoose.model('surveys');
 
@@ -12,9 +14,15 @@ module.exports = app => {
             title,
             subject,
             body,
-            recipients: recipients.split(',').map(email => ({ email: email.trim() })),
+            recipients: recipients
+                .split(',')
+                .map(email => ({ email: email.trim() })),
             _user: req.user.id,
             dateSent: Date.now()
         });
+
+        // Sending an email!
+        const mailer = new Mailer(survey, surveyTemplate(survey));
+        mailer.send();
     });
 };
