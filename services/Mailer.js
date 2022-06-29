011687a5377cb8f32038bf9ccdecaf7b7102e479
Ievgeniia Taranenko <ievgeniia.taranenko@adidas.com>
2022-06-29 13:45:51 +0200

    extracting recipients from sub document collection, pulling the email from every recipient, formatting it with helper.Email

--- services/Mailer.js
+++ services/Mailer.js
@@ -11,6 +11,12 @@ class Mailer extends helper.Mail {
         this.body = new helper.Content('text/html', content);
         this.recipients = this.formatAddresses(recipients);
     }
+
+    formatAddresses(recipients) {
+        return recipients.map(({ email }) => {
+            return new helper.Email(email);
+        });
+    }
 }
 
 module.exports = Mailer;
