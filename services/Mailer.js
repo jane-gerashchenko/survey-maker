e7adc3ba8575d8e4f72551853f783c6a5786ea39
Ievgeniia Taranenko <ievgeniia.taranenko@adidas.com>
2022-06-29 14:02:39 +0200

    added mailer properties

--- services/Mailer.js
+++ services/Mailer.js
@@ -10,12 +10,33 @@ class Mailer extends helper.Mail {
         this.subject = subject;
         this.body = new helper.Content('text/html', content);
         this.recipients = this.formatAddresses(recipients);
+
+        this.addContent(this.body);
+        this.addClickTracking();
+        this.addRecipients();
     }
 
     formatAddresses(recipients) {
         return recipients.map(({ email }) => {
             return new helper.Email(email);
         });
+    }
+
+    addClickTracking() {
+        const trackingSettings = new helper.TrackingSettings();
+        const clickTracking = new helper.ClickTracking(true, true);
+
+        trackingSettings.setClickTracking(clickTracking);
+        this.addTrackingSettings(trackingSettings);
+    }
+
+    addRecipients() {
+        const personalize = new helper.Personalization();
+        
+        this.recipients.forEach(recipients => {
+            personalize.addTo(recipients);
+        });
+        this.addPersonalization(personalize);
     }
 }
 
