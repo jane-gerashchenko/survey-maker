cd9910eb80bba32c852d58a60d909d53698e1d16
Ievgeniia Taranenko <ievgeniia.taranenko@adidas.com>
2022-06-30 10:36:24 +0200

    draft of the surveyForm component

---
+++ client/src/components/surveys/SurveyForm.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+
+class SurveyForm extends Component {
+    render() {
+        return (
+            <div>
+                SurveyForm!
+            </div>
+        );
+    }
+}
+
+export default reduxForm({
+    form: 'surveyForm'
+})(SurveyForm);
--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react'
+// SurveyNew shows SurveyForm and SurveyFormReview
+import React, { Component } from 'react';
+import SurveyForm from './SurveyForm';
 
 class SurveyNew extends Component {
     render() {
         return (
             <div>
-                SurveyNew!
+                <SurveyForm />
             </div>
         );
     }
