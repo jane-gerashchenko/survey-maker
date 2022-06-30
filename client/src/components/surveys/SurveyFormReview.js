fb90ebc1f73bbf4ca0d58844e43d622c4b8028ce
Ievgeniia Taranenko <ievgeniia.taranenko@adidas.com>
2022-06-30 15:04:23 +0200

    navigation between SurveyForm and SurveyFormReview

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,10 +1,13 @@
 // SurveyFormReview shows users their form inputs for review
 import React from "react";
 
-const SurveyFormReview = () => {
+const SurveyFormReview = ({ onCancel }) => {
     return (
         <div>
             <h5>Please confirm your entries</h5>
+            <button className="yellow darken-3 btn-flat" onClick={onCancel}>
+                Back
+            </button>
         </div>
     );
 };
