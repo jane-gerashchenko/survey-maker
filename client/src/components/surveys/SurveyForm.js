4bcd0ed16d39d0cd753fef576e9e8e3e90a6d80b
Ievgeniia Taranenko <ievgeniia.taranenko@adidas.com>
2022-06-30 11:51:50 +0200

    refactored rendering fields

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,38 +1,24 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 import SurveyField from './SurveyField';
 
+const FIELDS = [
+    { label: 'Survey Title', name: 'title' },
+    { label: 'Subject Line', name: 'subject' },
+    { label: 'Email Body', name: 'body' },
+    { label: 'Recipient List', name: 'emails' }
+];
+
 class SurveyForm extends Component {
     renderFields() {
-        return (
-            <div>
-                <Field 
-                    label="Survey Title" 
-                    type="text" 
-                    name="title" 
-                    component={SurveyField}
-                />
-                <Field 
-                    label="Subject Line" 
-                    type="text" 
-                    name="subject" 
-                    component={SurveyField}
-                />
-                <Field 
-                    label="Email Body" 
-                    type="text" 
-                    name="body" 
-                    component={SurveyField}
-                />
-                <Field 
-                    label="Recipient List" 
-                    type="text" 
-                    name="emails" 
-                    component={SurveyField}
-                />
-            </div>
-        );
+        return _.map(FIELDS, ({ label, name}) => {
+            return (
+            <Field key={name} component={SurveyField} type="text" label={label} name={name} />
+            );
+        });
     }
+
     render() {
         return (
             <div>
