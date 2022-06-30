f5dc6885ad0f9a393061cb8735c36eabac6bd584
Ievgeniia Taranenko <ievgeniia.taranenko@adidas.com>
2022-06-30 13:59:22 +0200

    empty field validation is shown firs by moving email validation to the top

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -42,13 +42,13 @@ class SurveyForm extends Component {
 function validate(values) {
     const errors = {};
 
+    errors.emails = validateEmails(values.emails || '');
+
     _.each(FIELDS, ({ name }) => {
         if (!values[name]) {
             errors[name] = 'You must provide a value'
         }
     });
-
-    errors.emails = validateEmails(values.emails || '');
 
     return errors;
 }
