b27150126ce97a853ebe5eed8fa13fe2df0edf7f
Ievgeniia Taranenko <ievgeniia.taranenko@adidas.com>
2022-06-30 11:39:03 +0200

    draft of the survey form

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,11 +1,45 @@
 import React, { Component } from 'react';
-import { reduxForm } from 'redux-form';
+import { reduxForm, Field } from 'redux-form';
+import SurveyField from './SurveyField';
 
 class SurveyForm extends Component {
+    renderFields() {
+        return (
+            <div>
+                <Field 
+                    label="Survey Title" 
+                    type="text" 
+                    name="title" 
+                    component={SurveyField}
+                />
+                <Field 
+                    label="Subject Line" 
+                    type="text" 
+                    name="subject" 
+                    component={SurveyField}
+                />
+                <Field 
+                    label="Email Body" 
+                    type="text" 
+                    name="body" 
+                    component={SurveyField}
+                />
+                <Field 
+                    label="Recipient List" 
+                    type="text" 
+                    name="emails" 
+                    component={SurveyField}
+                />
+            </div>
+        );
+    }
     render() {
         return (
             <div>
-                SurveyForm!
+                <form onSubmit={this.props.handleSubmit(values => console.log("values ", values))}>
+                    {this.renderFields()}
+                    <button type="submit">Submit</button>
+                </form>
             </div>
         );
     }
