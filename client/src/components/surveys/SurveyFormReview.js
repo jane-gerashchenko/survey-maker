d0637817893e41b92b22dfd1b47eb5c78b673331
Ievgeniia Taranenko <ievgeniia.taranenko@adidas.com>
2022-06-30 15:51:42 +0200

    displaying fields with values taken from state

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,10 +1,25 @@
 // SurveyFormReview shows users their form inputs for review
+import _ from 'lodash';
 import React from "react";
+import { connect } from "react-redux";
+import formFields from "./formFields";
 
-const SurveyFormReview = ({ onCancel }) => {
+const SurveyFormReview = ({ onCancel, formValues }) => {
+    const reviewFields = _.map(formFields, ({ name, label }) => {
+        return (
+            <div key={name}>
+                <label>{label}</label>
+                <div>
+                    {formValues[name]}
+                </div>
+            </div>
+        );
+    });
+
     return (
         <div>
             <h5>Please confirm your entries</h5>
+            {reviewFields}
             <button className="yellow darken-3 btn-flat" onClick={onCancel}>
                 Back
             </button>
@@ -12,4 +27,8 @@ const SurveyFormReview = ({ onCancel }) => {
     );
 };
 
-export default SurveyFormReview;
+function mapStateToProps(state) {
+    return { formValues: state.form.surveyForm.values };
+}
+
+export default connect(mapStateToProps)(SurveyFormReview);
