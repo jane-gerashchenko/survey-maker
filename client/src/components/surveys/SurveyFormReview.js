9d0e11cf0f3b92ca57b5d5d4f7f3844780f86036
Ievgeniia Taranenko <ievgeniia.taranenko@adidas.com>
2022-06-30 14:50:49 +0200

    draft of surveyForm Review component

---
+++ client/src/components/surveys/SurveyFormReview.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SurveyFormReview = () => {
+    return (
+        <div>
+            <h5>Please confirm your entries</h5>
+        </div>
+    );
+};
+
+export default SurveyFormReview;
