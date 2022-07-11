a1dd532d094c9faf9c1fc30f4f7133c5d98842b1
Ievgeniia Taranenko <ievgeniia.taranenko@adidas.com>
2022-07-11 16:50:54 +0200

    parsing the route and filtering out uniq events

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,6 @@
+const _ = require('lodash');
+const { Path } = require('path-parser');
+const { URL } = require('url');
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
@@ -12,7 +15,20 @@ module.exports = app => {
     });
 
     app.post('/api/surveys/webhooks', (req,res) => {
-        console.log('req.body', req.body);
+        const events = _.map(req.body, ({ email, url }) => {
+            const pathname = new URL(url).pathname;
+            const p = new Path('/api/surveys/:surveyId/:choice');
+            const match = p.test(pathname);
+            if (match) {
+                return { email, surveyId: match.surveyId, choice:match.choice };
+            }
+        });
+
+        const compactEvents = _.compact(events);
+        const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId');
+
+        console.log(uniqueEvents);
+
         res.send({});
     });
 
