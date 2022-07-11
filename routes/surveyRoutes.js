db5f77bcdb36ee6e05e4e6bcd2d5fe0266c39752
Ievgeniia Taranenko <ievgeniia.taranenko@adidas.com>
2022-07-11 16:53:56 +0200

    refactoring

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -15,10 +15,10 @@ module.exports = app => {
     });
 
     app.post('/api/surveys/webhooks', (req,res) => {
+        const p = new Path('/api/surveys/:surveyId/:choice');
+
         const events = _.map(req.body, ({ email, url }) => {
-            const pathname = new URL(url).pathname;
-            const p = new Path('/api/surveys/:surveyId/:choice');
-            const match = p.test(pathname);
+            const match = p.test(new URL(url).pathname);
             if (match) {
                 return { email, surveyId: match.surveyId, choice:match.choice };
             }
