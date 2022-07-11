4cb94b0742ca1e416dce03fff0583409719e297c
Ievgeniia Taranenko <ievgeniia.taranenko@adidas.com>
2022-07-11 17:08:40 +0200

    refactoring using _.chain()

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -17,17 +17,18 @@ module.exports = app => {
     app.post('/api/surveys/webhooks', (req,res) => {
         const p = new Path('/api/surveys/:surveyId/:choice');
 
-        const events = _.map(req.body, ({ email, url }) => {
-            const match = p.test(new URL(url).pathname);
-            if (match) {
-                return { email, surveyId: match.surveyId, choice:match.choice };
-            }
-        });
+       const events = _.chain(req.body)
+            .map(({ email, url }) => {
+                const match = p.test(new URL(url).pathname);
+                if (match) {
+                    return { email, surveyId: match.surveyId, choice:match.choice };
+                }
+            })
+            .compact()
+            .uniqBy('email', 'surveyId')
+            .value();
 
-        const compactEvents = _.compact(events);
-        const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId');
-
-        console.log(uniqueEvents);
+        console.log(events);
 
         res.send({});
     });
