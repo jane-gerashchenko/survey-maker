4f6e062f23beb8d043b768d85397df525fbbcd85
Ievgeniia Taranenko <ievgeniia.taranenko@adidas.com>
2022-07-12 13:44:36 +0200

    typo fix

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -41,7 +41,7 @@ module.exports = app => {
                     }
                 }, {
                     $inc: { [choice]: 1 },
-                    $set: { 'recipients.$.responsed': true },
+                    $set: { 'recipients.$.responded': true },
                     lastResponded: new Date()
                 }).exec();
             })
