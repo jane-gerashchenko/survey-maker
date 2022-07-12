48b3897a824318a4fc9cb79f1d4575ea648a00fe
Ievgeniia Taranenko <ievgeniia.taranenko@adidas.com>
2022-07-12 10:36:35 +0200

    updating the record in mongodb with survey response and fixed a route for 'thank you' message

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -10,14 +10,14 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-    app.get('/api/surveys/thanks', (req, res) => {
+    app.get('/api/surveys/:surveyId/:choice', (req, res) => {
         res.send('Thanks for voting!');
     });
 
     app.post('/api/surveys/webhooks', (req,res) => {
         const p = new Path('/api/surveys/:surveyId/:choice');
 
-       const events = _.chain(req.body)
+        _.chain(req.body)
             .map(({ email, url }) => {
                 const match = p.test(new URL(url).pathname);
                 if (match) {
@@ -26,9 +26,19 @@ module.exports = app => {
             })
             .compact()
             .uniqBy('email', 'surveyId')
+            .each(({ surveyId, email, choice }) => {
+                Survey.updateOne({
+                    _id: surveyId,
+                    recipients: {
+                        $elemMatch: { email: email, responded: false }
+                    }
+                }, {
+                    $inc: { [choice]: 1 },
+                    $set: { 'recipients.$.responsed': true },
+                    lastResponded: new Date()
+                }).exec();
+            })
             .value();
-
-        console.log(events);
 
         res.send({});
     });
