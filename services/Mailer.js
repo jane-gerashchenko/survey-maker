5d92950fd9202e0a10fac88151ab3550b18a7051
Ievgeniia Taranenko <ievgeniia.taranenko@adidas.com>
2022-07-12 13:20:52 +0200

    temporary changing email to test prod

--- services/Mailer.js
+++ services/Mailer.js
@@ -7,7 +7,7 @@ class Mailer extends helper.Mail {
         super();
 
         this.sgApi = sendgrid(keys.sendGridKey);
-        this.from_email = new helper.Email('no-reply@surveymaker.com');
+        this.from_email = new helper.Email('ievgeniia.taranenko@gmail.com');
         this.subject = subject;
         this.body = new helper.Content('text/html', content);
         this.recipients = this.formatAddresses(recipients);
